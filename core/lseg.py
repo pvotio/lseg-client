3c16adb8d7e3761c8529dcf087580736efc2875c
Ali Moghimi <mghmiali@gmail.com>
2025-06-23 14:29:29 +0000

    feat: multiprocessing implementation

--- core/lseg.py
+++ core/lseg.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import threading
 from urllib.parse import urljoin
 
@@ -14,6 +15,7 @@ class LSEG:
         logger.info("Initializing LSEG instance")
         self.request = Request().request
         self.result = {}
+        self.tasks = []
 
     def run(self):
         logger.info("LSEG run started")
@@ -21,8 +23,8 @@ class LSEG:
         self.tasks = self.rics.copy()
         logger.info("Fetched %d RICs", len(self.rics))
         self.start_workers()
-        logger.info("All threads have completed")
-        return self.result
+        logger.info("All processes and threads have completed")
+        return dict(self.result)
 
     def fetch_tickers(self):
         url = urljoin(self.BASE_URL, "esgsearchsuggestions/")
@@ -37,56 +39,73 @@ class LSEG:
             raise
 
     def start_workers(self):
-        logger.info("Starting %d worker threads", self.THREAD_COUNT)
-        threads = []
-        for i in range(self.THREAD_COUNT):
-            t = threading.Thread(target=self.worker, name=f"Worker-{i}")
-            threads.append(t)
+        manager = multiprocessing.Manager()
+        self.tasks = manager.list(self.tasks)
+        self.result = manager.dict()
+        lock = manager.RLock()
+        n_proc = multiprocessing.cpu_count()
+        logger.info(
+            "Starting %d processes × %d threads each", n_proc, self.THREAD_COUNT
+        )
+        processes = [
+            multiprocessing.Process(
+                target=self._process_target,
+                name=f"Proc-{i}",
+                args=(lock,),
+            )
+            for i in range(n_proc)
+        ]
+
+        for p in processes:
+            p.start()
+            logger.debug("Started %s", p.name)
+
+        for p in processes:
+            p.join()
+            logger.debug("%s has finished", p.name)
+
+    def _process_target(self, lock):
+        local_threads = [
+            threading.Thread(
+                target=self.worker,
+                name=f"{multiprocessing.current_process().name}-T{t}",
+                args=(lock,),
+                daemon=True,
+            )
+            for t in range(self.THREAD_COUNT)
+        ]
+
+        for t in local_threads:
             t.start()
-            logger.debug("Started thread %s", t.name)
 
-        for t in threads:
+        for t in local_threads:
             t.join()
-            logger.debug("Thread %s has finished", t.name)
 
-    def worker(self):
-        logger.debug("Thread %s: started", threading.current_thread().name)
-        while self.tasks:
-            try:
+    def worker(self, lock):
+        thread_name = threading.current_thread().name
+        logger.debug("%s: started", thread_name)
+
+        while True:
+            with lock:
+                if not self.tasks:
+                    logger.debug("%s: no more tasks", thread_name)
+                    break
                 ric_data = self.tasks.pop(0)
-            except IndexError:
-                logger.debug(
-                    "Thread %s: no more tasks", threading.current_thread().name
-                )
-                return
 
             ric = tuple(ric_data.values())
             if ric in self.result:
-                logger.debug(
-                    "Thread %s: skipping duplicate RIC %s",
-                    threading.current_thread().name,
-                    ric,
-                )
+                logger.debug("%s: skipping duplicate RIC %s", thread_name, ric)
                 continue
 
             try:
-                logger.debug(
-                    "Thread %s: fetching ESG scores for RIC %s",
-                    threading.current_thread().name,
-                    ric[1],
-                )
-                self.result[ric] = self.fetch_esg_scores(ric[1])
-                logger.debug(
-                    "Thread %s: fetched data for %s",
-                    threading.current_thread().name,
-                    ric[1],
-                )
+                logger.debug("%s: fetching ESG scores for %s", thread_name, ric[1])
+                data = self.fetch_esg_scores(ric[1])
+                with lock:
+                    self.result[ric] = data
+                logger.debug("%s: fetched data for %s", thread_name, ric[1])
             except Exception as e:
                 logger.warning(
-                    "Thread %s: Unable to fetch data for %s: %s",
-                    threading.current_thread().name,
-                    ric[1],
-                    str(e),
+                    "%s: unable to fetch data for %s: %s", thread_name, ric[1], e
                 )
 
     def fetch_esg_scores(self, ric):
